4a9604b65b69896e5941807b948d950b53bae504
Maxikk26 <maximiliano.bogo@gmail.com>
2020-09-24 16:30:50 -0400

    MOD change of class name and window redirect

---
+++ Frontend/AgregarMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Directorio.Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Directorio.Frontend
+{
+    /// <summary>
+    /// Lógica de interacción para AgregarMenu.xaml
+    /// </summary>
+    public partial class AgregarMenu : Window
+    {
+        Conexion pg;
+        public AgregarMenu(Conexion con)
+        {
+            InitializeComponent();
+            pg = con;
+        }
+
+        private void btnmedico_Click(object sender, RoutedEventArgs e)
+        {
+            AgregarMedico med = new AgregarMedico(pg);
+            med.Show();
+            this.Close();
+        }
+
+        private void btnespecialidad_Click(object sender, RoutedEventArgs e)
+        {
+            AgregarEspecialidad esp = new AgregarEspecialidad(pg);
+            esp.Show();
+            this.Close();
+        }
+
+        private void btnconsultorio_Click(object sender, RoutedEventArgs e)
+        {
+            AgregarConsultorio con = new AgregarConsultorio(pg);
+            con.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Frontend/MenuPrincipal.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Directorio.Backend;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Directorio.Frontend
+{
+    /// <summary>
+    /// Lógica de interacción para MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        Conexion pg;
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        public MainMenu(Conexion con)
+        {
+            InitializeComponent();
+            this.pg = con;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Delete delete = new Delete(pg);
+            delete.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AgregarMenu insert = new AgregarMenu(pg);
+            insert.Show();
+            this.Close();
+        }
+    }
+}
