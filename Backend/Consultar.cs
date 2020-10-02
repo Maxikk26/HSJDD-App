76926e1a252412e57eced36813ebb828b5d9dd59
Maxikk26 <maximiliano.bogo@gmail.com>
2020-10-02 11:50:50 -0400

    MOD consult of telefono for different query

--- Backend/Consultar.cs
+++ Backend/Consultar.cs
@@ -78,11 +78,16 @@ namespace Directorio.Backend
             string sql = "SELECT ME.id_medico, ME.p_nombre, ME.p_apellido, ME.s_apellido, traercorreos(ME.id_medico) FROM medico ME";
             ejecutar(sql);
         }
-        public void telefonos(string nombre)
+        public void telefonos(string nombre,bool medico)
         {
             if (pg.start())
             {
-                string sql = "SELECT ME.id_medico,TE.telefono,ME.p_nombre,ME.p_apellido,ME.s_apellido FROM medico ME, telefono TE WHERE ME.id_medico = TE.medico_id AND ME.p_nombre LIKE '"+nombre+"%'";
+                string sql;
+                if(medico)
+                    sql = "SELECT ME.id_medico,TE.telefono,ME.p_nombre,ME.p_apellido,ME.s_apellido FROM medico ME, telefono TE WHERE ME.id_medico = TE.medico_id AND ME.p_nombre LIKE '"+nombre+"%'";
+                else
+                    sql = "SELECT TE.id_telefono,TE.telefono,ME.p_nombre,ME.p_apellido,ME.s_apellido FROM medico ME, telefono TE WHERE ME.id_medico = TE.medico_id AND ME.p_nombre LIKE '" + nombre + "%'";
+
                 da = new NpgsqlDataAdapter(sql, pg.conn);
                 ds.Reset();
                 da.Fill(ds);
@@ -152,7 +157,7 @@ namespace Directorio.Backend
         {
             if (pg.start())
             {
-                string sql = "SELECT CO.numero AS consultorio,CO.referencia,PI.piso,PI.numero FROM consultorio CO, piso PI WHERE CO.piso_id = PI.id_piso AND CO.numero LIKE'" + nombre + "%' ";
+                string sql = "SELECT CO.id_consultorio,CO.numero AS consultorio,CO.referencia,PI.piso,PI.numero FROM consultorio CO, piso PI WHERE CO.piso_id = PI.id_piso AND CO.numero LIKE'" + nombre + "%' ";
                 da = new NpgsqlDataAdapter(sql, pg.conn);
                 ds.Reset();
                 da.Fill(ds);
